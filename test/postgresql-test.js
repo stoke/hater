dce3bf1175128125e983928c0b46f3cff633867d
yawnt <yawn.localhost@gmail.com>
2012-08-04 12:00:19 +0200

    [api] createTable

--- test/postgresql-test.js
+++ test/postgresql-test.js
@@ -2,18 +2,35 @@ var builder = require('../lib/builders/postgresql'),
     exec    = require('child_process').exec,
     assert  = require('assert');
 
-builder.connect('tcp://postgres@127.0.0.1/test');
+
+before(function(done) {
+  builder.connect('tcp://postgres@127.0.0.1/test');
+  done();
+});  
 
 describe('postgresql builder', function() {
 
+  describe('#insert', function() {
+  
+    it('should insert row into the db', function(done) {
+      builder
+        .insert('test', { test: 'burp' })
+        .exec(function(e, res) {
+          assert.equal(e, null);
+          done();
+        });
+    });
+
+  });
+
   describe('#select', function() {
 
-    it('should select row from db', function(done) {
+    it('should select row from the db', function(done) {
       builder
         .select('test', '*')
-        .where({test: 'harpdarp'})
+        .where({test: 'burp'})
         .exec(function(e, res) {
-          res.length.should.equal(3);
+          res.length.should.equal(2);
           assert.equal(e, null);
           done();
         }); 
