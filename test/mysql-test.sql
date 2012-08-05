9d147857415c86128dbe2d33a87c8c1fd93bc5d7
stoke <stoke95@yahoo.it>
2012-08-05 18:09:45 +0300

    Update test/mysql-test.sql

--- test/mysql-test.sql
+++
@@ -1,5 +0,0 @@
-create database if not exists test;
-use test;
-create table if not exists test ( id int auto_increment primary key, test varchar(32) );
-INSERT INTO test VALUES (null, 'yagxen');
-INSERT INTO test VALUES (null, 'yagonimod');
