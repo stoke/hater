ac57830f467438526205413badcd1acb2791422c
yawnt <yawn.localhost@gmail.com>
2012-08-05 19:41:05 +0200

    [fix] tests

---
+++ test/hater-test.js
@@ -0,0 +1,22 @@
+var assert = require('assert'),
+    hater  = require('../lib/hater');
+
+describe('hater', function() {
+  
+  describe('#extend', function() {
+    
+    it('should have .extend', function() {
+      hater.extend.should.be.a('function');
+    });
+
+  });
+
+  describe('#dialect', function() {
+
+    it('should have .dialect', function() {
+      hater.dialect.should.be.a('function');
+    });
+
+  });
+
+});
