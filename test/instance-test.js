1d21f3617272c39f06836cae0f419a4a4f75064b
stoke <stoke95@yahoo.it>
2012-08-06 22:25:25 +0300

    [fix] tests

--- test/instance-test.js
+++ test/instance-test.js
@@ -29,5 +29,54 @@ describe('Instance', function() {
 
   });
 
+  describe('find', function() {
+    it('should find rows', function(done) {
+      Model.find('*', function(e, r) {
+        expect(e).to.not.be.ok();
+        expect(r).to.be.an('array');
+        done();
+      });
+    });
+  });
+
+  describe('#save', function() {
+    it('should create a row when called without id', function(done) {
+      var model = new Model({test: 'lol'});
+      model.save(function(e, r) {
+        expect(e).not.to.be.ok();
+        done();
+      });
+    });
+
+    it('should save the row when called from find (with id)', function(done) {
+      Model.find('*', function(e, r) {
+        r[0].set('test', 'testa');
+        r[0].save(function(e, r) {
+          expect(e).not.to.be.ok();
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#destroy', function() {
+    it('should destroy the row when called from find (with id)', function(done) {
+      Model.find('*', function(e, r) {
+        r[0].destroy(function(e, r) {
+          expect(e).not.to.be.ok();
+          done();
+        });
+      });
+    });
+
+    it('should destroy the row', function(done) {
+      Model.find('*', function(e, r) {
+        Model.destroy({id: r[0].get('id')}, function(e, r) {
+          expect(e).not.to.be.ok();
+          done();
+        });
+      });
+    });
+  });
 
 });
