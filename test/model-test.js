f93d2a2c54ef5665917ebc9cff6598686e93f839
stoke <stoke95@yahoo.it>
2012-08-06 03:19:44 +0200

    [fix] tests

--- test/model-test.js
+++ test/model-test.js
@@ -17,6 +17,16 @@ describe('model', function() {
     });
   });
 
+  describe('find', function() {
+    it('should find rows', function(done) {
+      Model.find('*', function(e, r) {
+        expect(e).to.not.be.ok();
+        expect(r).to.be.an('array');
+        done();
+      });
+    });
+  });
+
   describe('#save', function() {
     it('should create a row when called without id', function(done) {
       var model = new Model({test: 'lol'});
@@ -30,6 +40,26 @@ describe('model', function() {
       Model.find('*', function(e, r) {
         r[0].set('test', 'testa');
         r[0].save(function(e, r) {
+          expect(e).not.to.be.ok();
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#destroy', function() {
+    it('should destroy the row when called from find (with id)', function(done) {
+      Model.find('*', function(e, r) {
+        r[0].destroy(function(e, r) {
+          expect(e).not.to.be.ok();
+          done();
+        });
+      });
+    });
+
+    it('should destroy the row', function(done) {
+      Model.find('*', function(e, r) {
+        Model.destroy({id: r[0].get('id')}, function(e, r) {
           expect(e).not.to.be.ok();
           done();
         });
