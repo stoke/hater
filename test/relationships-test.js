07e3daf8b85173894d78a18b299f51cb7cd405d6
yawnt <yawn.localhost@gmail.com>
2012-08-10 18:36:19 +0200

    [feature] hasOne

--- test/relationships-test.js
+++ test/relationships-test.js
@@ -26,13 +26,36 @@ before(function() {
 describe('relationships', function() {
 
   describe('hasOne', function() {
+    
+    var Model;
+
+    before(function(done) {
+      new Relationship({test: 'asd'}).save(function(e, instance) {
+        Model = instance;
+        done();
+      });
+    });
 
     it('should inject correct methods', function() {
-      var Model = new Relationship();
-
       Model.createRelationship2.should.be.a('function');
       Model.getRelationship2s.should.be.a('function');    
       Relationship._relationships.hasOne.indexOf('relationship2').should.not.equal(-1);
+    });
+
+    it('should enable child creation', function(done) {
+      Model.createRelationship2({
+        testo: 'testoz'
+      }, function(e, instance) {
+        instance.get('testo').should.equal('testoz');
+        done();
+      });
+    });
+
+    it('should enable child lookup', function(done) {
+      Model.getRelationship2s(function(e, instances) {
+        instances[0].get('testo').should.equal('testoz');
+        done();
+      });
     });
 
   });
