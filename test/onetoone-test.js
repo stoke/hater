f3d48ba716dcb9e9e2f1dc0a132acb66f85eb021
yawnt <yawn.localhost@gmail.com>
2012-08-11 15:48:01 +0200

    [fix] tests

--- test/onetoone-test.js
+++ test/onetoone-test.js
@@ -1,7 +1,7 @@
 var hater  = require('../lib/hater'),
     assert = require('assert');
 
-hater.dialect('mysql', 'mysql://root@127.0.0.1/test');
+hater.dialect('postgresql', 'tcp://postgres@127.0.0.1/test');
 
 var Model = hater.extend('model', {});
 
@@ -26,13 +26,13 @@ describe('relationships', function() {
   describe('oneToOne', function() {
 
     it('should setup right fields in Models', function() {
-      Model._schema.otherId.should.equal('INT');
-      Other._schema.modelId.should.equal('INT');
+      Model._schema.other_id.should.equal('INT');
+      Other._schema.model_id.should.equal('INT');
     });
     
 
     it('should load with fetch', function(done) {
-      new Model({test: 'asd'}).save(function(e, instance) {
+      new Model({test: 1}).save(function(e, instance) {
         Model.find({where: {id: instance.get('id')}, fetch: ["others"]}, function(e, res) {
           assert.equal(true, Array.isArray(res[0].others));
           done();
