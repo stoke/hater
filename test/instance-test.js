d2bc63acdc8a7db5bdaf18e45796c4f9e3e2ad0b
stoke <stoke95@yahoo.it>
2012-08-13 01:43:39 +0200

    [feature] #findOne and #findOrCreate

--- test/instance-test.js
+++ test/instance-test.js
@@ -2,7 +2,7 @@ var hater  = require('../lib/hater'),
     expect = require('expect.js'),
     assert = require('assert');
 
-hater.dialect('mysql', 'mysql://root@localhost/test');
+hater.dialect('mysql', 'mysql://root:toor@localhost/test');
 
 var Model = hater.extend('instance', {});
 
@@ -78,6 +78,44 @@ describe('Instance', function() {
           expect(e).not.to.be.ok();
           done();
         });
+      });
+    });
+  });
+
+  describe('#findOrCreate', function() {
+    it('should find when a row exist', function(done) {
+      Model.findOrCreate({ test: 'asd' }, function(e, r) {
+        Model.findOrCreate({ test: 'asd' }, function(e, ro) {
+          expect(r[0].get('id')).to.be.eql(ro[0].get('id'));
+          done();
+        });
+      });
+    });
+
+    it("should create when a row doesn't exist", function(done) {
+      Model.findOrCreate({ test: 'test' }, function(e, r) {
+        expect(e).to.not.be.ok();
+        expect(r).to.be.an('array');
+        expect(r[0].get('test')).to.be.eql('test');
+        done();
+      });
+    })
+  });
+
+  describe('#findOne', function() {
+    it('should return first element when a row exist', function(done) {
+      Model.findOne({where: {test: 'asd'}}, function(e, r) {
+        expect(e).to.not.be.ok();
+        expect(r).to.not.be.an('array');
+        done();
+      });
+    });
+    
+    it('should return null when a row does not exist', function(done) {
+      Model.findOne({where: {test: 'unexistent'}}, function(e, r) {
+        expect(e).to.not.be.ok();
+        expect(r).to.not.be.ok();
+        done();
       });
     });
   });
